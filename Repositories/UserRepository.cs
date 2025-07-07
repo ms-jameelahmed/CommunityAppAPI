c9a863577b39227c3e513c9dd75d9aae9a783fd3
Mohammed Althaf Hussain <althafhussain25@gmail.com>
2025-07-08 01:09:32 +0530

    2nd Push

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -7,6 +7,7 @@ namespace CommunityAppAPI.Repositories
     {
         public User ValidateUser(string email, string password)
         {
+            // Second Push
             //sample code from AH. Dated: 08  jul 2025
             // Mock user validation
             if (email == "admin@example.com" && password == "password")

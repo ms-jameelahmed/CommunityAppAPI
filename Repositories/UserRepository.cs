212108b2f863cb1c0334280979b6c03e4549aaa1
Mohammed Althaf Hussain <althafhussain25@gmail.com>
2025-07-08 00:54:01 +0530

    Sample Code check in

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -7,6 +7,7 @@ namespace CommunityAppAPI.Repositories
     {
         public User ValidateUser(string email, string password)
         {
+            //sample code from AH
             // Mock user validation
             if (email == "admin@example.com" && password == "password")
                 return new User { Id = 1, Email = email };
